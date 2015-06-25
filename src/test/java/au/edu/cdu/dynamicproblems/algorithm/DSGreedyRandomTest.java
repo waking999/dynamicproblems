caad755953a61ab5326a876c5aed2bb1da3ff8ad
waking999 <waking.wang@gmail.com>
2015-06-25 19:18:00 +0930

    workspace3

--- src/test/java/au/edu/cdu/dynamicproblems/algorithm/DSGreedyRandomTest.java
+++ src/test/java/au/edu/cdu/dynamicproblems/algorithm/DSGreedyRandomTest.java
@@ -61,20 +61,22 @@ public class DSGreedyRandomTest {
 
 	}
 
-	@Ignore
+	// @Ignore
 	@Test
 	public void testKONET() throws InterruptedException, IOException,
 			FileNotFoundException {
-		String destFile = "out/output-DSRandomGreedy-KONET.csv";
+		String destFile = "out/output-DSRandomGreedy-KONECT.csv";
 
 		String path = "src/test/resources/KONET/";
-		String[] files = { "000027_zebra.konet", "000034_zachary.konet",
-				"000062_dolphins.konet", "000112_David_Copperfield.konet",
-				"000198_Jazz_musicians.konet",
-				// "000212_pdzbase.konet",
-				"001133_rovira.konet",
-				// "001174_euroroad.konet",
-				"001858_hamster.konet"
+		String[] files = {  "000027_zebra.konet",
+		 "000034_zachary.konet",
+		 "000062_dolphins.konet",
+		 "000112_David_Copperfield.konet",
+		 "000198_Jazz_musicians.konet",
+				"000212_pdzbase.konet",
+				 "001133_rovira.konet",
+				"001174_euroroad.konet",
+		 "001858_hamster.konet"
 		// "002426_hamster_ful.konet",
 		// "002888_facebook.konet",
 		// "003133_Human_protein_Vidal.konet",
@@ -83,12 +85,27 @@ public class DSGreedyRandomTest {
 		// "010680_Pretty_Good_Privacy.konet",
 		// "006474_Route_views.konet"
 		};
-		int[] times = { 10, 100, 1000 };
-		for (String file : files) {
-			runDSGreedyRandom(path + file, destFile, times);
+		// int[] times = { 10, 100, 1000 };
+		// for (String file : files) {
+		// runDSGreedyRandom(path + file, destFile, times);
+		// }
+		//int[] times = { 10, 100, 1000 };
+		int[] times = { 1000};
+		for (int j = 0; j < times.length; j++) {
+			
+			for (int i = 2; i <= 10; i++) {
+				log.debug(i+"----------------------");
+				if (destFile != null) {
+					FileOperation.saveCVSFile(destFile, i+"----------------------");
+				}
+				for (String file : files) {
+					runDSGreedyRandom(path + file, destFile, times[j]);
+				}
+			}
 		}
 	}
-@Ignore
+
+	@Ignore
 	@Test
 	public void testDIMACS() throws InterruptedException, IOException,
 			FileNotFoundException {
@@ -115,54 +132,38 @@ public class DSGreedyRandomTest {
 			runDSGreedyRandom(path + file, destFile, times);
 		}
 	}
-@Test
-public void testBHOSLIB() throws InterruptedException, IOException,
-		FileNotFoundException {
-	String destFile = "out/output-DSRandomGreedy-BHOSLIB.csv";
 
-	String path = "src/test/resources/BHOSLIB/";
-	String[] files = { "frb30-15-mis/frb30-15-1.mis",
-			"frb30-15-mis/frb30-15-2.mis",
-			"frb30-15-mis/frb30-15-3.mis",
-			"frb30-15-mis/frb30-15-4.mis",
-			"frb30-15-mis/frb30-15-5.mis",
-			"frb35-17-mis/frb35-17-1.mis",
-			"frb35-17-mis/frb35-17-2.mis",
-			"frb35-17-mis/frb35-17-3.mis",
-			"frb35-17-mis/frb35-17-4.mis",
-			"frb35-17-mis/frb35-17-5.mis",
-			"frb40-19-mis/frb40-19-1.mis",
-			"frb40-19-mis/frb40-19-2.mis",
-			"frb40-19-mis/frb40-19-3.mis",
-			"frb40-19-mis/frb40-19-4.mis",
-			"frb40-19-mis/frb40-19-5.mis",
-			"frb45-21-mis/frb45-21-1.mis",
-			"frb45-21-mis/frb45-21-2.mis",
-			"frb45-21-mis/frb45-21-3.mis",
-			"frb45-21-mis/frb45-21-4.mis",
-			"frb45-21-mis/frb45-21-5.mis",
-			"frb53-24-mis/frb53-24-1.mis",
-			"frb53-24-mis/frb53-24-2.mis",
-			"frb53-24-mis/frb53-24-3.mis",
-			"frb53-24-mis/frb53-24-4.mis",
-			"frb53-24-mis/frb53-24-5.mis",
-			"frb56-25-mis/frb56-25-1.mis",
-			"frb56-25-mis/frb56-25-2.mis",
-			"frb56-25-mis/frb56-25-3.mis",
-			"frb56-25-mis/frb56-25-4.mis",
-			"frb56-25-mis/frb56-25-5.mis",
-			"frb59-26-mis/frb59-26-1.mis",
-			"frb59-26-mis/frb59-26-2.mis",
-			"frb59-26-mis/frb59-26-3.mis",
-			"frb59-26-mis/frb59-26-4.mis",
-			"frb59-26-mis/frb59-26-5.mis"
-	};
-	int[] times = { 10, 100, 1000 };
+	@Ignore
+	@Test
+	public void testBHOSLIB() throws InterruptedException, IOException,
+			FileNotFoundException {
+		String destFile = "out/output-DSRandomGreedy-BHOSLIB.csv";
 
-	for (String file : files) {
-		runDSGreedyRandom(path + file, destFile, times);
+		String path = "src/test/resources/BHOSLIB/";
+		String[] files = { "frb30-15-mis/frb30-15-1.mis",
+				"frb30-15-mis/frb30-15-2.mis", "frb30-15-mis/frb30-15-3.mis",
+				"frb30-15-mis/frb30-15-4.mis", "frb30-15-mis/frb30-15-5.mis",
+				"frb35-17-mis/frb35-17-1.mis", "frb35-17-mis/frb35-17-2.mis",
+				"frb35-17-mis/frb35-17-3.mis", "frb35-17-mis/frb35-17-4.mis",
+				"frb35-17-mis/frb35-17-5.mis", "frb40-19-mis/frb40-19-1.mis",
+				"frb40-19-mis/frb40-19-2.mis", "frb40-19-mis/frb40-19-3.mis",
+				"frb40-19-mis/frb40-19-4.mis", "frb40-19-mis/frb40-19-5.mis",
+				"frb45-21-mis/frb45-21-1.mis", "frb45-21-mis/frb45-21-2.mis",
+				"frb45-21-mis/frb45-21-3.mis", "frb45-21-mis/frb45-21-4.mis",
+				"frb45-21-mis/frb45-21-5.mis", "frb53-24-mis/frb53-24-1.mis",
+				"frb53-24-mis/frb53-24-2.mis", "frb53-24-mis/frb53-24-3.mis",
+				"frb53-24-mis/frb53-24-4.mis", "frb53-24-mis/frb53-24-5.mis",
+				"frb56-25-mis/frb56-25-1.mis", "frb56-25-mis/frb56-25-2.mis",
+				"frb56-25-mis/frb56-25-3.mis", "frb56-25-mis/frb56-25-4.mis",
+				"frb56-25-mis/frb56-25-5.mis", "frb59-26-mis/frb59-26-1.mis",
+				"frb59-26-mis/frb59-26-2.mis", "frb59-26-mis/frb59-26-3.mis",
+				"frb59-26-mis/frb59-26-4.mis", "frb59-26-mis/frb59-26-5.mis" };
+		int[] times = { 10, 100, 1000 };
+
+		for (String file : files) {
+			runDSGreedyRandom(path + file, destFile, times);
+		}
 	}
-}
 
 	private void runDSGreedyRandom(String inputFile, String destFile,
 			int[] times) throws InterruptedException, IOException,
@@ -203,6 +204,47 @@ public void testBHOSLIB() throws InterruptedException, IOException,
 			if (destFile != null) {
 				FileOperation.saveCVSFile(destFile, message);
 			}
+		}
+
+	}
+
+	private void runDSGreedyRandom(String inputFile, String destFile, int times)
+			throws InterruptedException, IOException, FileNotFoundException {
+
+		int finalSize = Integer.MAX_VALUE;
+		List<Integer> finalDs = null;
+
+		FileOperation fo = IOUtil.getProblemInfoByEdgePair(inputFile);
+		List<String[]> am = fo.getAdjacencyMatrix();
+
+		long start = System.nanoTime();
+
+		for (int i = 0; i < times; i++) {
+			Graph<Integer, Integer> g = AlgorithmUtil.prepareGraph(am);
+
+			DSGreedyRandom ag = new DSGreedyRandom(g);
+			ag.run();
+
+			List<Integer> ds = ag.getDominatingSet();
+			// Assert.assertTrue(AlgorithmUtil.isDS(g, ds));
+
+			// Result r = ag.getResult();
+			// log.debug(r.getString());
+
+			int dsSize = ds.size();
+
+			if (dsSize < finalSize) {
+				finalSize = dsSize;
+				finalDs = ds;
+			}
+
+		}
+		long end = System.nanoTime();
+		String message = inputFile + "," + times + "," + finalSize + ","
+				+ (end - start);
+		LogUtil.printResult(message, finalDs);
+		if (destFile != null) {
+			FileOperation.saveCVSFile(destFile, message);
 		}
 
 	}
